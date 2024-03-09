30f6943f3617b663fed30c5500de3f94c60978e5
cherrycheeks <dirtiestcontext@gmail.com>
2024-03-09 12:34:11 -0500

    comitting inital stuff

---
+++ src/main/groovy/com/dirtiestcontext/template/TemplateApplication.groovy
@@ -0,0 +1,13 @@
+package com.dirtiestcontext.template
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TemplateApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TemplateApplication, args)
+	}
+
+}
